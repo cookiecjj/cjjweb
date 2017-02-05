43c31879735bb1e5d1d459ee2f62a0e1da2a0a91
cookiecjj <yeshuang@yeshuangdeMacBook-Air.local>
2017-02-05 14:36:34 +0800

    txt

--- kunRosedu/js/index.js
+++ kunRosedu/js/index.js
@@ -1,15 +1,47 @@
 
-
-
-//按钮居中
-function buttonCenter(){
+/*添加外观效果*/
+function featureBind()
+{
+	/*绑定阴影效果*/
+	$("body .main_content_mid").on("mouseover",function(){
+		   
+		  $(this).css("box-shadow","0px 10px 30px   #505050")
+	 	
+	})
+	$(".main_content_mid").on("mouseleave",function(){
+		
+		  $(this).css("box-shadow","0px 10px 30px   #808080");
+	 
+	})
 	
+	
+	 
+	
+	
+}
+
+/*给定指定元素背景*/
+function backgroundBind()
+{  
+     var len=$(".main_content_mid").length;
+   	 for(var i=0;i<len;i++)
+   	 {
+   	 	 $(".main_content_mid:eq("+i+")").css("background","url(img/background/"+ i%7+".jpg) no-repeat");	
+   	 	  $(".main_content_mid:eq("+i+")").css("background-size","cover");	
+   	  
+   	 	
+   	 }
+   	 
+   	
+ 
+  
 	
 }
 
 
 
 $(function(){
-	alert($('#cc').width())
 	
-})
+	featureBind();
+	backgroundBind()
+});
